ac744e87a3b895f2be194efaf9954a7ce6a5d1af
bohdansolovey <37744768+bohdansolovey@users.noreply.github.com>
2019-02-06 13:36:13 +0200

    Update urls.py

--- restFull/urls.py
+++ restFull/urls.py
@@ -1,22 +1,5 @@
-"""restFull URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import include, path # new
-from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -25,9 +8,7 @@ from rest_framework_simplejwt.views import (
 from posts import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
-
 schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
@@ -35,11 +16,9 @@ urlpatterns = [
     path('api/', include('posts.urls')),
     path('api/', include('users.urls')),
     path('admin/', admin.site.urls),
-    #url(r'^refresh-token/', refresh_jwt_token),
-    #path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('docs', schema_view)
+    path('docs', schema_view, name='docs')
 ]
 
 if settings.DEBUG:
