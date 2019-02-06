4a6cad17f7f531b58c42e7f60ec305dfc1f0a162
bohdansolovey <37744768+bohdansolovey@users.noreply.github.com>
2019-02-06 13:40:57 +0200

    Update serializers.py

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,13 +1,9 @@
-# posts/serializers
 from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
+    img = serializers.ImageField(use_url=True, allow_null=True, max_length= None)
     class Meta:
         model = Post
-        fields = ('id','title', 'body','author',
-                #'img'
-
-                 )
-
+        fields = ('id','title', 'body','author','img')
