44c3c754fbfa1f96197ba97a454d9f19430d35d9
bohdansolovey <37744768+bohdansolovey@users.noreply.github.com>
2019-02-06 13:45:42 +0200

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_jwt.views import refresh_jwt_token
-
 from users import views
 
 urlpatterns = [
@@ -10,6 +8,5 @@ urlpatterns = [
     path('users/posts', views.PostByAuthor.as_view(),name='user-post-list'),
     path('auth/', include('rest_auth.urls')),
     path('auth/registration/', include('rest_auth.registration.urls')),
-    path('refresh-token/', refresh_jwt_token),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
